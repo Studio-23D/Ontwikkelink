80c36c8b53195639e5c3c12efaa90ae7629314d5
Wesley Cats <wesleycats@gmail.com>
2020-04-08 14:49:49 +0200

    Connection Points (v1.3.1)
    Added
    - Changable variables to set an amount of connection points on the node manager for testing reasons
    - Struct for the styles of the node and connection points
    - Struct for the actions of the node and connection points
    
    Enabled
    - Node draw functionality for testing reasons
    - Node drag functionality
    - NodeManager functionality for testing reasons
    
    Fixed
    - Connection point positions bug, where the positions were out of line with different amounts of points
    
    Changed
    - Code structure to the prototype structure for testing reasons

--- Assets/Scripts/NodeSystem/Element/Node/ColorNode.cs
+++ Assets/Scripts/NodeSystem/Element/Node/ColorNode.cs
@@ -6,9 +6,38 @@ public class ColorNode : Node
 {
     private Color color = Color.white;
 
-	public ColorNode()
+	public ColorNode(Vector2 position, float width, float height, Style style, Actions actions, int pointsInAmount, int pointsOutAmount)
 	{
-		
+		this.title = "Color";
+		this.rect = new Rect(position.x, position.y, width, height);
+
+		this.style.nodeStyle = style.nodeStyle;
+		this.style.defaultNodeStyle = style.nodeStyle;
+		this.style.selectedNodeStyle = style.selectedNodeStyle;
+
+		this.actions.OnRemoveNode = actions.OnRemoveNode;
+
+		if (pointsInAmount > pointsOutAmount)
+		{
+			SetHeight(pointsInAmount);
+		}
+		else
+		{
+			SetHeight(pointsOutAmount);
+		}
+
+		for (int i = 0; i < pointsInAmount; i++)
+		{
+			float pointHeight = rect.height + (i * (rect.height / pointsInAmount));
+			ConnectionPoint input = new ConnectionPoint(this, ConnectionPointType.In, style.pointStyle, pointHeight, actions.OnClickConnectionPoint);
+			this.inPoints.Add(input);
+		}
+
+		for (int i = 0; i < pointsOutAmount; i++)
+		{
+			float pointHeight = rect.height + (i * (rect.height / pointsOutAmount));
+			ConnectionPoint output = new ConnectionPoint(this, ConnectionPointType.Out, style.pointStyle, pointHeight, actions.OnClickConnectionPoint);
+			this.outPoints.Add(output);
+		}
 	}
 }
-
--- Assets/Scripts/NodeSystem/NodeManager.cs
+++ Assets/Scripts/NodeSystem/NodeManager.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class NodeManager : MonoBehaviour
 {
+	/*
     private List<Node> node;
     private SystemEventHandeler eventHandeler;
+
     void Init()
     {
         node = new List<Node>();
@@ -24,242 +26,239 @@ public class NodeManager : MonoBehaviour
 
         SystemEventHandeler.onRemoveNode.Invoke();
     }
+}*/
+
+
+	[SerializeField]
+	private Texture2D nodeBackground;
+	[SerializeField]
+	private Texture2D pointBackground;
+
+	[SerializeField]
+	private int pointsInAmount;
+	[SerializeField]
+	private int pointsOutAmount;
+
+	private List<Node> nodes;
+	private List<Connection> connections;
+
+	private GUIStyle nodeStyle;
+	private GUIStyle selectedNodeStyle;
+	private GUIStyle pointStyle;
+	private GUIStyle inPointStyle;
+	private GUIStyle outPointStyle;
+
+	private ConnectionPoint selectedInPoint;
+	private ConnectionPoint selectedOutPoint;
+
+	private void OnEnable()
+	{
+		nodeStyle = new GUIStyle();
+		nodeStyle.normal.background = nodeBackground;
+		nodeStyle.border = new RectOffset(12, 12, 12, 12);
+
+		selectedNodeStyle = new GUIStyle();
+		selectedNodeStyle.normal.background = nodeBackground;
+		selectedNodeStyle.border = new RectOffset(12, 12, 12, 12);
+
+		pointStyle = new GUIStyle();
+		pointStyle.normal.background = pointBackground;
+		pointStyle.active.background = pointBackground;
+		pointStyle.border = new RectOffset(4, 4, 12, 12);
+
+		inPointStyle = new GUIStyle();
+		inPointStyle.normal.background = pointBackground;
+		inPointStyle.active.background = pointBackground;
+		inPointStyle.border = new RectOffset(4, 4, 12, 12);
+
+		outPointStyle = new GUIStyle();
+		outPointStyle.normal.background = pointBackground;
+		outPointStyle.active.background = pointBackground;
+		outPointStyle.border = new RectOffset(4, 4, 12, 12);
+	}
+
+	private void OnGUI()
+	{
+		DrawNodes();
+		DrawConnections();
+
+		DrawConnectionLine(Event.current);
+
+		ProcessNodeEvents(Event.current);
+		ProcessEvents(Event.current);
+	}
+
+	private void DrawNodes()
+	{
+		if (nodes != null)
+		{
+			for (int i = 0; i < nodes.Count; i++)
+			{
+				nodes[i].Draw();
+			}
+		}
+	}
+
+	private void DrawConnections()
+	{
+		if (connections != null)
+		{
+			for (int i = 0; i < connections.Count; i++)
+			{
+				connections[i].Draw();
+			}
+		}
+	}
+
+	private void DrawConnectionLine(Event e)
+	{
+		if (selectedInPoint != null && selectedOutPoint == null)
+		{
+			Handles.DrawBezier(
+				selectedInPoint.rect.center,
+				e.mousePosition,
+				selectedInPoint.rect.center + Vector2.left * 50f,
+				e.mousePosition - Vector2.left * 50f,
+				Color.white,
+				null,
+				2f
+			);
+
+			GUI.changed = true;
+		}
+
+		if (selectedOutPoint != null && selectedInPoint == null)
+		{
+			Handles.DrawBezier(
+				selectedOutPoint.rect.center,
+				e.mousePosition,
+				selectedOutPoint.rect.center - Vector2.left * 50f,
+				e.mousePosition + Vector2.left * 50f,
+				Color.white,
+				null,
+				2f
+			);
+
+			GUI.changed = true;
+		}
+	}
+
+	private void ProcessEvents(Event e)
+	{
+		switch (e.type)
+		{
+			case EventType.MouseDown:
+				if (e.button == 1)
+				{
+					ProcessContextMenu(e.mousePosition);
+				}
+				break;
+		}
+	}
+
+	private void ProcessNodeEvents(Event e)
+	{
+		if (nodes != null)
+		{
+			for (int i = nodes.Count - 1; i >= 0; i--)
+			{
+				bool guiChanged = nodes[i].ProcessEvents(e);
+
+				if (guiChanged)
+				{
+					GUI.changed = true;
+				}
+			}
+		}
+	}
+
+	private void ProcessContextMenu(Vector2 mousePosition)
+	{
+		GenericMenu genericMenu = new GenericMenu();
+		genericMenu.AddItem(new GUIContent("Add node"), false, () => OnClickAddNode(mousePosition));
+		genericMenu.ShowAsContext();
+	}
+
+	private void OnClickAddNode(Vector2 mousePosition)
+	{
+		if (nodes == null)
+		{
+			nodes = new List<Node>();
+		}
+
+		Node.Style nodeStyleContainer = new Node.Style();
+		nodeStyleContainer.nodeStyle = nodeStyle;
+		nodeStyleContainer.selectedNodeStyle = selectedNodeStyle;
+		nodeStyleContainer.pointStyle = pointStyle;
+
+		Node.Actions actionsContainer = new Node.Actions();
+		actionsContainer.OnRemoveNode = OnClickRemoveNode;
+		actionsContainer.OnClickConnectionPoint = OnClickOutPoint;
+
+		nodes.Add(new ColorNode(mousePosition, 200, 50, nodeStyleContainer, actionsContainer, pointsInAmount, pointsOutAmount));
+
+		//nodes.Add(new ColorNode(mousePosition, 200, 50, nodeStyle, selectedNodeStyle, inPointStyle, outPointStyle, OnClickInPoint, OnClickOutPoint, OnClickRemoveNode));
+	}
+
+	private void OnClickInPoint(ConnectionPoint inPoint)
+	{
+		selectedInPoint = inPoint;
+
+		if (selectedOutPoint != null)
+		{
+			if (selectedOutPoint.node != selectedInPoint.node)
+			{
+				CreateConnection();
+				ClearConnectionSelection();
+			}
+			else
+			{
+				ClearConnectionSelection();
+			}
+		}
+	}
+
+	private void OnClickOutPoint(ConnectionPoint outPoint)
+	{
+		selectedOutPoint = outPoint;
+
+		if (selectedInPoint != null)
+		{
+			if (selectedOutPoint.node != selectedInPoint.node)
+			{
+				CreateConnection();
+				ClearConnectionSelection();
+			}
+			else
+			{
+				ClearConnectionSelection();
+			}
+		}
+	}
+
+	private void OnClickRemoveConnection(Connection connection)
+	{
+		connections.Remove(connection);
+	}
+
+	private void CreateConnection()
+	{
+		if (connections == null)
+		{
+			connections = new List<Connection>();
+		}
+
+		connections.Add(new Connection(selectedInPoint, selectedOutPoint, OnClickRemoveConnection));
+	}
+
+	private void ClearConnectionSelection()
+	{
+		selectedInPoint = null;
+		selectedOutPoint = null;
+	}
+
+	private void OnClickRemoveNode(Node node)
+	{
+		nodes.Remove(node);
+	}
 }
-
-/*
- * [SerializeField]
-    private Texture2D nodeBackground;
-    [SerializeField]
-    private Texture2D pointBackground;
-
-    private List<Node> nodes;
-    private List<Connection> connections;
-
-    private GUIStyle nodeStyle;
-    private GUIStyle selectedNodeStyle;
-    private GUIStyle inPointStyle;
-    private GUIStyle outPointStyle;
-
-    private ConnectionPoint selectedInPoint;
-    private ConnectionPoint selectedOutPoint;
-
-    private void OnEnable()
-    {
-        nodeStyle = new GUIStyle();
-        nodeStyle.normal.background = nodeBackground;
-        nodeStyle.border = new RectOffset(12, 12, 12, 12);
-
-        selectedNodeStyle = new GUIStyle();
-        selectedNodeStyle.normal.background = nodeBackground;
-        selectedNodeStyle.border = new RectOffset(12, 12, 12, 12);
-
-        inPointStyle = new GUIStyle();
-        inPointStyle.normal.background = pointBackground;
-        inPointStyle.active.background = pointBackground;
-        inPointStyle.border = new RectOffset(4, 4, 12, 12);
-
-        outPointStyle = new GUIStyle();
-        outPointStyle.normal.background = pointBackground;
-        outPointStyle.active.background = pointBackground;
-        outPointStyle.border = new RectOffset(4, 4, 12, 12);
-    }
-
-    private void OnGUI()
-    {
-        DrawNodes();
-        DrawConnections();
-
-        DrawConnectionLine(Event.current);
-
-        ProcessNodeEvents(Event.current);
-        ProcessEvents(Event.current);
-    }
-
-    private void DrawNodes() 
-    {
-        if (nodes != null)
-        {
-            for (int i = 0; i < nodes.Count; i++)
-            {
-                nodes[i].Draw();
-            }
-        }
-    }
-
-    private void DrawConnections()
-    {
-        if(connections != null)
-        {
-            for(int i = 0; i < connections.Count; i++)
-            {
-                connections[i].Draw();
-            }
-        }
-    }
-
-    private void DrawConnectionLine(Event e)
-    {
-        if (selectedInPoint != null && selectedOutPoint == null)
-        {
-            Handles.DrawBezier(
-                selectedInPoint.rect.center,
-                e.mousePosition,
-                selectedInPoint.rect.center + Vector2.left * 50f,
-                e.mousePosition - Vector2.left * 50f,
-                Color.white,
-                null,
-                2f
-            );
-
-            GUI.changed = true;
-        }
-
-        if (selectedOutPoint != null && selectedInPoint == null)
-        {
-            Handles.DrawBezier(
-                selectedOutPoint.rect.center,
-                e.mousePosition,
-                selectedOutPoint.rect.center - Vector2.left * 50f,
-                e.mousePosition + Vector2.left * 50f,
-                Color.white,
-                null,
-                2f
-            );
-
-            GUI.changed = true;
-        }
-    }
-
-    private void ProcessEvents(Event e)
-    {
-        switch (e.type)
-        {
-            case EventType.MouseDown:
-                if (e.button == 1)
-                {
-                    ProcessContextMenu(e.mousePosition);
-				}
-                break;
-        }
-    }
-
-    private void ProcessNodeEvents(Event e)
-    {
-        if (nodes != null)
-        {
-            for (int i = nodes.Count - 1; i >= 0; i--)
-            {
-                bool guiChanged = nodes[i].ProcessEvents(e);
-
-                if (guiChanged)
-                {
-                    GUI.changed = true;
-                }
-            }
-        }
-    }
-
-    private void ProcessContextMenu(Vector2 mousePosition)
-    {
-        GenericMenu genericMenu = new GenericMenu();
-        genericMenu.AddItem(new GUIContent("Add node"), false, () => OnClickAddNode(mousePosition));
-        genericMenu.ShowAsContext();
-    }
-
-    private void OnClickAddNode(Vector2 mousePosition)
-    {
-        if (nodes == null)
-        {
-            nodes = new List<Node>();
-        }
-
-        nodes.Add(new ColorNode(mousePosition, nodeStyle, selectedNodeStyle, inPointStyle, outPointStyle, OnClickInPoint, OnClickOutPoint, OnClickRemoveNode));
-    }
-
-    private void OnClickInPoint(ConnectionPoint inPoint)
-    {
-        selectedInPoint = inPoint;
-
-        if(selectedOutPoint != null)
-        {
-            if(selectedOutPoint.node != selectedInPoint.node)
-            {
-                CreateConnection();
-                ClearConnectionSelection();
-            }
-            else
-            {
-                ClearConnectionSelection();
-            }
-        }
-    }
-
-    private void OnClickOutPoint(ConnectionPoint outPoint)
-    {
-        selectedOutPoint = outPoint;
-
-        if(selectedInPoint != null)
-        {
-            if (selectedOutPoint.node != selectedInPoint.node)
-            {
-                CreateConnection();
-                ClearConnectionSelection();
-            }
-            else
-            {
-                ClearConnectionSelection();
-            }
-        }
-    }
-
-    private void OnClickRemoveConnection(Connection connection)
-    {
-        connections.Remove(connection);
-    }
-
-    private void CreateConnection()
-    {
-        if(connections == null)
-        {
-            connections = new List<Connection>();
-        }
-
-        connections.Add(new Connection(selectedInPoint, selectedOutPoint, OnClickRemoveConnection));
-    }
-
-    private void ClearConnectionSelection()
-    {
-        selectedInPoint = null;
-        selectedOutPoint = null;
-    }
-
-    private void OnClickRemoveNode(Node node)
-    {
-        if(connections != null)
-        {
-            List<Connection> connectionsToRemove = new List<Connection>();
-
-            for(int i = 0; i < connections.Count; i++)
-            {
-				foreach (ConnectionPoint point in node.inPoints)
-				{
-					connectionsToRemove.Add(connections[i]);
-				}
-
-				foreach (ConnectionPoint point in node.outPoints)
-				{
-					connectionsToRemove.Add(connections[i]);
-				}
-
-				if (connections[i].inPoint == node.inPoint || connections[i].outPoint == node.outPoint)
-                {
-                    connectionsToRemove.Add(connections[i]);
-                }
-
-connectionsToRemove = null;
-            }
-
-            nodes.Remove(node);
-        }
-    }
-    */
