c7fd958cdaeb739efff875f8a13962d8d130b177
gwnrobin <robinschellingerhout@live.nl>
2020-05-19 14:31:31 +0200

    Textile Node update (V3.1.2.1)
    - Added update to textile node
    - Added bugfix for spawing multiple apearanceItems

---
+++ Assets/Scripts/Enums/TextileData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "TextileData", menuName = "NodeSystem/TextileData", order = 1)]
+public class TextileData : ScriptableObject
+{
+    public Texture2D albedoMap;
+    public Texture2D normalMap;
+    public Texture2D maskMap;
+
+}
